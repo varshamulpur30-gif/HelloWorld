1c84478a3d618a1d0374d497e44cbc70e0df2eea
Mulpuru Varsha <YOUR_GITHUB_EMAIL@gmail.com>
2026-07-05 20:30:31 +0530

    Signed-off-by: Mulpuru Varsha <YOUR_GITHUB_EMAIL@gmail.com>
    Added CommandLine2 program

---
+++ src/CommandLine2.java
@@ -0,0 +1,13 @@
+public class CommandLine2 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 2) {
+            System.out.println(args[0] + " Technologies " + args[1]);
+        } else {
+            System.out.println("Please pass two command line arguments");
+        }
+
+    }
+
+}
