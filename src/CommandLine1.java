71f4f35c262fcb19f31a09fbf4701a422cbcf65c
Mulpuru Varsha <YOUR_GITHUB_EMAIL@gmail.com>
2026-07-05 10:49:05 +0530

    First commit

---
+++ src/CommandLine1.java
@@ -0,0 +1,10 @@
+public class CommandLine1 {
+    public static void main(String[] args) {
+
+        if (args.length == 2) {
+            System.out.println(args[0] + " Technologies " + args[1]);
+        } else {
+            System.out.println("Please pass two command line arguments.");
+        }
+    }
+}
